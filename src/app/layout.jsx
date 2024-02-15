72c0033cc70919a6b3c9cb1b12a4bad4503bf3f9
Lauty128 <lautarosilverii@gmail.com>
2024-02-15 13:22:14 -0300

    Tab image changed in Layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        <link rel="shortcut icon" href="logo-mini.png" type="image/png" />
+        <link rel="shortcut icon" href="/logo-light.png" type="image/png" />
       </head>
       <body className={inter.className}>
         {children}
