5282a427ba402ce38e921bd3d4964854f34a8c8d
Lautaro Silverii <lautarosilverii@gmail.com>
2025-08-01 20:34:06 -0300

    Actualizacion de informacion e imagenes

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -14,7 +14,7 @@ export function Header(){
             </div>
             <div className="Header__separator"></div>
             <div className="Header__descriptionContainer">
-                <p>Joven desarrollador de Argentina que se dedica a la creación de aplicaciones web, destinadas a solucionar un problema determinado o aumentar la productividad de una empresa; Capaz de adaptarse a distintos entornos y tecnologías.</p>
+                <p>Desarrollador web de Argentina, enfocado en crear soluciones digitales que resuelven problemas reales y optimizan procesos. Adaptable a distintos entornos y tecnologías.</p>
                 <p>Puedes echarle un vistazo a mi curriculum!!</p>
                 {/* <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, commodi. Necessitatibus consequuntur ipsa sint odio nihil architecto expedita reiciendis ducimus? Soluta, possimus eum magnam voluptatibus doloremque modi provident dolorum perferendis officiis odit pariatur nesciunt tempore. Non asperiores sequi ipsum repudiandae nulla recusandae.</p> */}
                 <a href="/curriculum.pdf" target="__blank" className="Button--viewMore">Ver curriculum</a>
--- src/components/static/Footer.jsx
+++ src/components/static/Footer.jsx
@@ -1,6 +1,6 @@
 //----- Assets
     import { FaLinkedin , FaGithub, FaInstagram, FaYoutube } from "react-icons/fa"
-
+    import { SiVsco } from "react-icons/si";
 
 export function Footer(){
 
@@ -9,17 +9,20 @@ export function Footer(){
             <span className="Footer__name">Lautaro Silverii</span>
 
             <div className="">
-                <a href="https://github.com/Lauty128" className="Footer__network">
+                <a href="https://github.com/Lauty128" className="Footer__network" title="Github">
                     <FaGithub />
                 </a>
-                <a href="https://www.linkedin.com/in/lautaro-silverii-8a5750209/" className="Footer__network">
+                <a href="https://www.linkedin.com/in/lautaro-silverii-8a5750209/" className="Footer__network" title="Linkedin">
                     <FaLinkedin />
                 </a>
-                <a href="https://www.instagram.com/silverii_lautaro03/" className="Footer__network">
+                <a href="https://www.instagram.com/silverii_lautaro03/" className="Footer__network" title="Instagram">
                     <FaInstagram />
                 </a>
-                <a href="https://www.youtube.com/channel/UCVxMhUWW3RQP_U8nt8Q8mLQ" className="Footer__network">
+                <a href="https://www.youtube.com/channel/UCVxMhUWW3RQP_U8nt8Q8mLQ" className="Footer__network" title="Youtube">
                     <FaYoutube />
+                </a>
+                <a href="https://vsco.co/silveriilautaro/gallery" className="Footer__network" title="Vsco">
+                    <SiVsco />
                 </a>
             </div>
         </div>
